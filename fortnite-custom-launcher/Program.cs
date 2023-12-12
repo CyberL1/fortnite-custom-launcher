5b154fb7b26984e97b5cdaef3f37e5fdbede5bbf
CyberL1 <mateusz.hf18@gmail.com>
2023-12-12 18:39:10 +0100

    Add config changing

--- fortnite-custom-launcher/Program.cs
+++ fortnite-custom-launcher/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Hosting;
 using fortnite_custom_launcher.Settings;
 using System.Diagnostics;
+using System.Text.Json;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder();
 builder.Configuration.Sources.Clear();
@@ -28,5 +29,20 @@ switch (option)
     case "1":
         Console.WriteLine($"Path: {config.Path}");
         Process.Start("cmd.exe", $"/c start {config.Path}/FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping.exe -epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -nobe -fromfl=eac -fltoken=24963ce04b575a5ca65526h0 -skippatchcheck -AUTH_LOGIN={config.Login} -AUTH_PASSWORD={config.Password} -AUTH_TYPE=epic");
+        break;
+    case "2":
+        Console.Write("Enter new game path: ");
+        config.Path = Console.ReadLine();
+
+        File.WriteAllText("settings.json", JsonSerializer.Serialize(config));
+        break;
+    case "3":
+        Console.Write("Login: ");
+        config.Login = Console.ReadLine();
+
+        Console.Write("Password: ");
+        config.Password = Console.ReadLine();
+
+        File.WriteAllText("settings.json", JsonSerializer.Serialize(config));
         break;
 }
