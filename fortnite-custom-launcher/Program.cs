3c367a0f74ad64c5869954f3d2454d86d6090bd8
CyberL1 <mateusz.hf18@gmail.com>
2023-12-12 17:53:49 +0100

    Add laucher settings and game running

--- fortnite-custom-launcher/Program.cs
+++ fortnite-custom-launcher/Program.cs
@@ -1,7 +1,32 @@
-﻿Console.WriteLine("Select an option\n");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using fortnite_custom_launcher.Settings;
+using System.Diagnostics;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+builder.Configuration.Sources.Clear();
+
+builder.Configuration.AddJsonFile("settings.json", false, true);
+
+Settings config = new();
+builder.Configuration.Bind(config);
+
+using IHost host = builder.Build();
+
+await host.StartAsync();
+
+Console.WriteLine("Select an option\n");
 
 Console.WriteLine("1. Run fortnite");
 Console.WriteLine("2. Change game path");
 Console.WriteLine("3. Change account credentials");
 
-Console.ReadLine();
+var option = Console.ReadLine();
+
+switch (option)
+{
+    case "1":
+        Console.WriteLine($"Path: {config.Path}");
+        Process.Start("cmd.exe", $"/c start {config.Path}/FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping.exe -epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -nobe -fromfl=eac -fltoken=24963ce04b575a5ca65526h0 -skippatchcheck -AUTH_LOGIN={config.Login} -AUTH_PASSWORD={config.Password} -AUTH_TYPE=epic");
+        break;
+}
---
+++ fortnite-custom-launcher/Settings.cs
@@ -0,0 +1,8 @@
+﻿namespace fortnite_custom_launcher.Settings;
+
+public sealed class Settings
+{
+    public string Path { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+}
