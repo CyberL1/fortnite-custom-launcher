06ea669d438dc5f1fc3cc3a08c72f51c40849e16
CyberL1 <mateusz.hf18@gmail.com>
2023-12-12 18:47:46 +0100

    Don't close launcher after changig settings

--- fortnite-custom-launcher/Program.cs
+++ fortnite-custom-launcher/Program.cs
@@ -16,33 +16,41 @@ using IHost host = builder.Build();
 
 await host.StartAsync();
 
-Console.WriteLine("Select an option\n");
-
-Console.WriteLine("1. Run fortnite");
-Console.WriteLine("2. Change game path");
-Console.WriteLine("3. Change account credentials");
-
-var option = Console.ReadLine();
-
-switch (option)
+void menu()
 {
-    case "1":
-        Console.WriteLine($"Path: {config.Path}");
-        Process.Start("cmd.exe", $"/c start {config.Path}/FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping.exe -epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -nobe -fromfl=eac -fltoken=24963ce04b575a5ca65526h0 -skippatchcheck -AUTH_LOGIN={config.Login} -AUTH_PASSWORD={config.Password} -AUTH_TYPE=epic");
-        break;
-    case "2":
-        Console.Write("Enter new game path: ");
-        config.Path = Console.ReadLine();
+    Console.Clear();
 
-        File.WriteAllText("settings.json", JsonSerializer.Serialize(config));
-        break;
-    case "3":
-        Console.Write("Login: ");
-        config.Login = Console.ReadLine();
+    Console.WriteLine("Select an option\n");
 
-        Console.Write("Password: ");
-        config.Password = Console.ReadLine();
+    Console.WriteLine("1. Run fortnite");
+    Console.WriteLine("2. Change game path");
+    Console.WriteLine("3. Change account credentials");
 
-        File.WriteAllText("settings.json", JsonSerializer.Serialize(config));
-        break;
+    var option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            Console.WriteLine($"Path: {config.Path}");
+            Process.Start("cmd.exe", $"/c start {config.Path}/FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping.exe -epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -nobe -fromfl=eac -fltoken=24963ce04b575a5ca65526h0 -skippatchcheck -AUTH_LOGIN={config.Login} -AUTH_PASSWORD={config.Password} -AUTH_TYPE=epic");
+            break;
+        case "2":
+            Console.Write("Enter new game path: ");
+            config.Path = Console.ReadLine();
+
+            File.WriteAllText("settings.json", JsonSerializer.Serialize(config));
+            menu();
+            break;
+        case "3":
+            Console.Write("Login: ");
+            config.Login = Console.ReadLine();
+
+            Console.Write("Password: ");
+            config.Password = Console.ReadLine();
+
+            File.WriteAllText("settings.json", JsonSerializer.Serialize(config));
+            break;
+    }
 }
+
+menu();
