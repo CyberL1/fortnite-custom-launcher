242f5112adaee7b99ee777c67aec56aef45cf1a3
CyberL1 <mateusz.hf18@gmail.com>
2023-12-12 16:15:29 +0100

    Dodaj pliki projektów.

---
+++ fortnite-custom-launcher/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Select an option\n");
+
+Console.WriteLine("1. Run fortnite");
+Console.WriteLine("2. Change game path");
+Console.WriteLine("3. Change account credentials");
+
+Console.ReadLine();
